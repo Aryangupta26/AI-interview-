5e0139fe796da74a09f9dbc884537d57cefd6f81
Aryangupta26 <147248884+Aryangupta26@users.noreply.github.com>
2026-03-25 01:55:45 +0530

    Update ServerUrl to production backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,8 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://ai-interview-backend-nmm4.onrender.com"
+
 
 function App() {
 
